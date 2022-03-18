00de02722a07cf4927a700db23fdfeea68b4f239
komase <eng00yoko@yahoo.co.jp>
2022-03-18 22:23:11 +0900

    use struct

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,38 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
-fn main() {
-    start_datagrid();
+struct DataGrid {
+    data: HashMap<String, String>,
 }
 
-fn handle_connection(mut stream: TcpStream) {
+impl DataGrid {
+    pub fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.data.insert(key, value);
+    }
+
+    pub fn get(&self, key: String) -> Option<&String> {
+        self.data.get(&key)
+    }
+}
+
+fn handle_connection(mut stream: TcpStream, data_grid: &mut DataGrid) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let message = String::from_utf8_lossy(&buffer[..]);
     let req: Vec<&str> = message.split(' ').collect();
-    let my_date = operate(String::from(req[1]), String::from(req[2]), req[0]);
-    println!("Request: {:?}", my_date);
+    operate(
+        String::from(req[1]),
+        String::from(req[2]),
+        req[0],
+        data_grid,
+    );
+    println!("Request: {:?}", data_grid.get("a".to_string()));
 }
 
 fn write() {
@@ -23,20 +44,22 @@ fn write() {
     stream.write(request.as_bytes()).unwrap();
 }
 
-fn start_datagrid() {
+fn start_datagrid(data_grid: &mut DataGrid) {
     let lister = TcpListener::bind("127.0.0.1:7878").unwrap();
     write();
     for stream in lister.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        handle_connection(stream, data_grid);
     }
 }
 
-fn operate(key: String, value: String, operation: &str) -> HashMap<String, String> {
-    let mut data: HashMap<String, String> = HashMap::new();
+fn operate(key: String, value: String, operation: &str, data_grid: &mut DataGrid) {
     if operation == "SET" {
-        data.insert(key, value);
+        data_grid.set(key, value);
     }
+}
 
-    return data;
+fn main() {
+    let mut data_grid = DataGrid::new();
+    start_datagrid(&mut data_grid);
 }
